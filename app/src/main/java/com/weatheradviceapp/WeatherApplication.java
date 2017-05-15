ee0d36f838faf49c99cc1a541a6154f3e95b0256
Jeroen Bobbeldijk <jeroen@devpros.nl>
2017-05-15 21:25:23 +0200

    Add Realm.io

---
+++ app/src/main/java/com/weatheradviceapp/WeatherApplication.java
@@ -0,0 +1,12 @@
+package com.weatheradviceapp;
+
+import android.app.Application;
+import io.realm.Realm;
+
+public class WeatherApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
